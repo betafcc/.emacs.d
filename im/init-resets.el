7291d530c824ac7c7364517cf4f1d8b329f7fc9c
Brendon <bfaccion@gmail.com>
2016-03-08 05:54:38 -0300

    Improves startup time

--- im/init-resets.el
+++ im/init-resets.el
@@ -1,3 +1,4 @@
+(setq inhibit-startup-message t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (put 'upcase-region 'disabled nil)
--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+(add-to-list 'load-path "~/.emacs.d/vendor/benchmark-init-el")
+(require 'benchmark-init-loaddefs)
+(benchmark-init/activate)
+
+
 (require 'package)
 (add-to-list 'package-archives
   '("melpa-stable" . "http://stable.melpa.org/packages/") t)
@@ -5,26 +10,6 @@
              '("melpa" . "http://melpa.org/packages/")
              t)
 (package-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-ghc-show-info t)
- '(custom-safe-themes
-   (quote
-    ("b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "135bbd2e531f067ed6a25287a47e490ea5ae40b7008211c70385022dbab3ab2a" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(inhibit-startup-buffer-menu nil)
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   (quote
-    (smart-mode-line-powerline-theme racket-mode helm undo-tree yascroll multiple-cursors neotree company-ghc ghc hindent haskell-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 
 (add-to-list 'load-path "~/.emacs.d/im")
@@ -42,8 +27,8 @@
 )		 
 
 
-(require 'init-haskell)
-(require 'init-racket)
+(eval-after-load 'haskell-mode '(require 'init-haskell))
+(eval-after-load 'racket-mode '(require 'init-racket))
 
 
 (provide 'init)
