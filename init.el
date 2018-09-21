7a5d33ae62645ffea7044665f82bed81ba9bcff7
Beta Faccion <betafcc@gmail.com>
2018-09-21 19:02:36 -0200

    changes

--- init.el
+++ init.el
@@ -1,2 +1,16 @@
+(package-initialize)
+
 (require 'org)
-(org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (ido-describe-bindings))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
