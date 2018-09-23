0e05fcdf7302dc4ec57b673ffa29628453f328ac
Beta Faccion <betafcc@gmail.com>
2018-09-23 11:18:37 -0200

    with use-package

--- init.el
+++ init.el
@@ -1,16 +1,54 @@
+(require 'package)
+
+(setq package-enable-at-startup nil)
+
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(require 'org)
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+(global-hl-line-mode 1)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq inhibit-splash-screen t)
+(setq make-backup-files nil)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(display-time-mode 1)
+(display-battery-mode 1)
+(setq column-number-mode t)
+
+
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode))
+
+(use-package solarized-theme
+  :ensure t
+  :config
+  (load-theme 'solarized-light t))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (ido-describe-bindings))))
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(package-selected-packages (quote (solarized-theme which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "pyrs" :family "Fira Code")))))
