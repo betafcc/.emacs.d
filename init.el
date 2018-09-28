6ef8db39f6fce67af32209cd1bbb3f7658b8b435
Beta Faccion <betafcc@gmail.com>
2018-09-28 17:54:04 -0200

    Begins literate config

--- init.el
+++ init.el
@@ -1,138 +1,41 @@
+;;; This pre-loading section is mostly from UncleDave
+;;; https://github.com/daedreth/UncleDavesEmacs/blob/master/init.el
+
+;;; This fixed garbage collection, makes emacs start up faster ;;;;;;;
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(defun startup/revert-file-name-handler-alist ()
+  (setq file-name-handler-alist startup/file-name-handler-alist))
+
+(defun startup/reset-gc ()
+  (setq gc-cons-threshold 16777216
+	gc-cons-percentage 0.1))
+
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;
 (require 'package)
-
 (setq package-enable-at-startup nil)
-
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
-(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+(setq package-archives '(("ELPA"  . "http://tromey.com/elpa/")
+			 ("gnu"   . "http://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")
+			 ("org"   . "https://orgmode.org/elpa/")
+			 ("SC"    . "http://joseito.republika.pl/sunrise-commander/")))
 (package-initialize)
+;;;
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; solves uggly modeline underline
-;; https://github.com/bbatsov/solarized-emacs/issues/187
-(setq x-underline-at-descent-line t)
-
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
-(setq inhibit-splash-screen t)
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(display-time-mode 1)
-(setq column-number-mode t)
-
-(defvar my-term-shell "/bin/bash")
-(defadvice ansi-term (before force-bash)
-  (interactive (list my-term-shell)))
-(ad-activate 'ansi-term)
-
-(global-set-key (kbd "<C-S-return>") 'ansi-term)
-
-(setq scroll-conservatively 100)
-(setq ring-bell-function 'ignore)
-
-;; emacs treats camelCase as single word, this fixes it
-(global-subword-mode 1)
-
-(show-paren-mode 1)
-(setq electric-pair-pairs
-      '((?\{ . ?\})
-        (?\( . ?\))
-        (?\[ . ?\])
-        (?\" . ?\")
-        ))
-(electric-pair-mode t)
-
-
-;; (global-prettify-symbols-mode t)
-
-
-(use-package which-key
-  :ensure t
-  :init
-  (which-key-mode))
-
-(use-package xclip
-  :ensure t
-  :init
-  (xclip-mode 1))
-
-(use-package solarized-theme
-  :ensure t
-  :init
-  (setq solarized-distinct-fringe-background t)
-  (setq solarized-high-contrast-mode-line t)
-  (setq solarized-use-more-italic t)
-  (setq solarized-scale-org-headlines t)
-  (load-theme 'solarized-light t))
-
-;; cursor stuff
-;; TODO: fix cursor color when launching from daemon
-;; https://emacs.stackexchange.com/questions/13291
-(global-hl-line-mode 1)
-(blink-cursor-mode -1)
-(setq-default cursor-type '(bar . 3))
-(set-cursor-color "#ff8676")
-
-
-(use-package beacon
-  :ensure t
-  :init
-  (beacon-mode 1)
-  :config
-  (setq beacon-color "#f8a53a"))
-
-(use-package centered-cursor-mode
-  :ensure t
-  :bind ("C-M-l" . 'centered-cursor-mode))
-
-
-(use-package rainbow-mode
-  :ensure t
-  :init
-    (add-hook 'prog-mode-hook 'rainbow-mode))
-
-(use-package fancy-battery
-  :ensure t
-  :config
-    (setq fancy-battery-show-percentage t)
-    (setq battery-update-interval 15)
-    (if window-system
-      (fancy-battery-mode)
-      (display-battery-mode)))
-
-(use-package org
-  :mode ("\\.org\\'"  . org-mode)
-  :config
-  (setq org-ellipsis "▼"))
-
-
-(use-package org-indent
-  :hook (org-mode . org-indent-mode))
-
-(use-package org-bullets
-  :ensure t
-  :config
-    (add-hook 'org-mode-hook (lambda () (org-bullets-mode))))
-
-(use-package ob
-  :defer t
-  :config
-  (setq org-confirm-babel-evaluate nil)
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((python . t)
-     (shell . t)
-     (js . t))))
-
-;; TODO: add a keybinding to it maybe?
-;; (add-to-list 'default-frame-alist '(undecorated . t))
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
